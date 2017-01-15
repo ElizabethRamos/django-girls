f50c024cf06310b8183e14564ec825368aa1753d
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-01-15 12:10:11 -0200

    My djangogirls app, first commit

---
+++ nome.py
@@ -0,0 +1,11 @@
+inp =raw_input ('Qual o seu peso?')
+peso = 59
+
+if peso < '59':
+   print('Pare de comer hamburger!')
+
+#elif peso > '59':
+#    print('Pare de comer')
+
+
+
--- teste.py
+++ teste.py
@@ -1,7 +1,4 @@
 inp =raw_input ("Eliza broca?")
-broca = "sim"
-
-
 
 print 'sim'
 
